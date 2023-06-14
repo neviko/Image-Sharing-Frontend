dd7cedb35149f63b815c31c5baddee9bb9337f4f
neviko <nevosayag@gmail.com>
2023-06-14 21:43:39 +0300

    remove unnecessary files

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-import FileSharing from "./components/FileSharing";
-
-const App = () => {
-  return <FileSharing />;
-};
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App";
+import FileSharing from "./components/FileSharing";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <FileSharing />
   </React.StrictMode>
 );
 
